cabe8ab02329ac544e55b8dfb72b823a827e26aa
Domingo <domingohui@gmail.com>
2017-03-25 19:42:17 -0400

    Testing bot

---
+++ bot.js
@@ -0,0 +1,29 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+const PORT = process.env.port || process.env.PORT || 3978;
+server.listen(PORT, function () {
+    console.log('%s listening to http://localhost:%s', server.name, PORT); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
