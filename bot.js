3525545befaf82736db989671f1dd6a1bc1c3b7e
Domingo <domingohui@gmail.com>
2017-03-26 00:34:51 -0400

    End point

--- bot.js
+++ bot.js
@@ -8,8 +8,11 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 const PORT = process.env.port || process.env.PORT || 3978;
+const END_POINT = '/api/messages';
+
 server.listen(PORT, function () {
     console.log('%s listening to http://localhost:%s', server.name, PORT); 
+    console.log('Bot message end point: http://localhost:%s%s', PORT, END_POINT);
 });
   
 // Create chat bot
@@ -18,7 +21,7 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+server.post(END_POINT, connector.listen());
 
 //=========================================================
 // Bots Dialogs
