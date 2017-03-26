ccf7c2ff5b98efb21f61cce3fe60eb23ef55615b
Domingo <domingohui@gmail.com>
2017-03-26 06:29:10 -0400

    Url image from bot

--- compare_image.py
+++ compare_image.py
@@ -1,10 +1,18 @@
 from skimage.measure import compare_ssim
+from skimage import io
 import numpy as np
 import cv2
 import sys
+import urllib.request
 
 def import_image_convert_grey (path):
-    img = cv2.imread(path)
+    img = None
+    if 'http:' in path:
+        img = io.imread(path)
+    else:
+        img = cv2.imread(path)
+
+    # Convert image to grey colour
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
